7976a3e3d50de1422c3645f509e14d9d087a0898
SAlkhairy <salkhairy@hotmail.com>
2016-11-03 17:27:53 +0300

    Thursday 3/11

--- Graduation_Project/accounts/forms.py
+++ Graduation_Project/accounts/forms.py
@@ -1,7 +1,20 @@
 from django.forms import ModelForm
-from accounts.models import Post
+from django.contrib.auth.models import User
+from accounts.models import Post, CommonProfile
+from django import formsclass
 
+UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class CommonProfileForm(forms.ModelForm):
+    class Meta:
+        model = CommonProfile
+        fields = ['human_name', 'pet_name', 'pet_type', 'profile_pic', 'interests']
+        
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['user','text','image','datetime']
+        fields = ['text', 'image']
